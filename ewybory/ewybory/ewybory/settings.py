bf03162f494435f922d12fae897c1c4101d8cbd4
gsenetra <fujaki9@gmail.com>
2019-11-25 17:41:09 +0100

    polaczenie z BD

--- ewybory/ewybory/ewybory/settings.py
+++ ewybory/ewybory/ewybory/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'ewybory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'senetrag',
+        'USER': 'senetrag',
+        'PASSWORD': 'qwerty123',
+        'HOST': 'bad.uwm.edu.pl',
     }
 }
 
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT'
 
 USE_I18N = True
 
