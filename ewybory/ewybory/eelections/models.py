fe48b6b85b0ef5dc7db5f62c905247f0c01d30e0
gsenetra <fujaki9@gmail.com>
2019-11-30 14:05:30 +0100

    models added

--- ewybory/ewybory/eelections/models.py
+++ ewybory/ewybory/eelections/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Voters(models.Model):
+    PESEL = models.IntegerField()
+    name = models.CharField(max_length=45)
+    surname = models.CharField(max_length=45)
+    login = models.CharField(max_length=45)
+    password = models.CharField(max_length=45)
+    commune = models.ForeignKey('Communes', on_delete=models.CASCADE)
+
+
+class Communes(models.Model):
+    name = models.CharField(max_length=45)
+    country = models.ForeignKey('Counties', on_delete=models.CASCADE)
+
+
+class Counties(models.Model):
+    name = models.CharField(max_length=45)
+    constituency = models.ForeignKey('Constituencies', on_delete=models.CASCADE)
+
+
+class Constituencies(models.Model):
+    voivodeship = models.CharField(max_length=45)
+
+
+class Candidates(models.Model):
+    number_on_list = models.IntegerField()
+    name = models.CharField(max_length=45)
+    surname = models.CharField(max_length=45)
+    number_of_votes = models.IntegerField()
+    election_committe = models.ForeignKey('ElectionCommittes', on_delete=models.CASCADE)
+    commune = models.ForeignKey('Communes', on_delete=models.CASCADE)
+
+
+class ElectionCommittes(models.Model):
+    name = models.CharField(max_length=45)
+    drawn_number = models.IntegerField()
+    number_of_votes = models.IntegerField()
+    nov_in_percents = models.FloatField()
+    
--- ewybory/ewybory/ewybory/settings.py
+++ ewybory/ewybory/ewybory/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'eelections.apps.EelectionsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
